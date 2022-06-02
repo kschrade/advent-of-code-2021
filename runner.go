9344076be601e05e68713c3eca63d2f2e1352333
kyleschrade <kyleschrade@stockx.com>
2022-06-02 10:04:11 -0400

    Added timer to each day

--- runner.go
+++ runner.go
@@ -1,23 +1,42 @@
 package main
 
 import (
-	"advent-of-code-2021/days"
 	"fmt"
+	"time"
+
+	"advent-of-code-2021/days"
 )
 
 func main() {
+	start := time.Now()
 	fmt.Println("day 1")
 	days.DayOneP1()
 	days.DayOneP2()
+	elapsed := time.Since(start)
+	fmt.Println("day 1 code took:", elapsed)
+	fmt.Println("")
+
+	start = time.Now()
 	fmt.Println("day 2")
 	days.DayTwoP1()
 	days.DayTwoP2()
+	elapsed = time.Since(start)
+	fmt.Println("")
+	fmt.Println("day 2 code took:", elapsed)
+
+	start = time.Now()
 	fmt.Println("")
 	fmt.Println("day 3")
 	days.DayThreeP1()
 	days.DayThreeP2()
+	elapsed = time.Since(start)
+	fmt.Println("day 3 code took:", elapsed)
+
+	start = time.Now()
 	fmt.Println("")
 	fmt.Println("day 4")
 	days.DayFourP1()
 	days.DayFourP2()
+	elapsed = time.Since(start)
+	fmt.Println("day 4 code took:", elapsed)
 }
